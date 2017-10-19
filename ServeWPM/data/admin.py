fc5071873d61131e5734961a22a306943e088ceb
ughe <ughe@users.noreply.github.com>
2017-10-19 12:34:23 -0400

    Switched admin

--- ServeWPM/data/admin.py
+++ ServeWPM/data/admin.py
@@ -3,23 +3,23 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import *
+# from .models import *
 
-__all__ = [
-    Crawl,
-    CrawlHistory,
-    FlashCookie,
-    HttpRequest,
-    HttpResponse,
-    Localstorage,
-    ProfileCookie,
-    SiteVisit,
-    Task,
-    Xpath,
-]
-#     HttpRequestCookie,
-#     HttpResponseCookie,
+# __all__ = [
+#     Crawl,
+#     CrawlHistory,
+#     FlashCookie,
+#     HttpRequest,
+#     HttpResponse,
+#     Localstorage,
+#     ProfileCookie,
+#     SiteVisit,
+#     Task,
+#     Xpath,
+# ]
+# #     HttpRequestCookie,
+# #     HttpResponseCookie,
 
-# Register your models here.
-for m in __all__:
-    admin.site.register(m);
+# # Register your models here.
+# for m in __all__:
+#     admin.site.register(m);
